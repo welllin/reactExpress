a18e98be28a073189ab348b3f43495df4c9f8681
Well <309635368@qq.com>
2015-09-03 22:39:50 +1200

    Bundling the application with browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp'),
     LiveServer = require('gulp-live-server'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    reactify = require('reactify');
 
 gulp.task('live-server', function(){
     var server = new LiveServer('server/main.js');
     server.start();
+});
+
+gulp.task('bundle', function(){
+    return browserify({
+        entries:'app/main.jsx',
+        debug: true
+    })
+    .transform(reactify)
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./.tmp'));
 });
 
 gulp.task('serve', ['live-server'], function(){
