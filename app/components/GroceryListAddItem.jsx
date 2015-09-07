385c1c2972fcebc77fd145e2c49c80a08851085f
Well <309635368@qq.com>
2015-09-07 22:09:38 +1200

    Using form in react Applications

--- app/components/GroceryListAddItem.jsx
+++ app/components/GroceryListAddItem.jsx
@@ -1,11 +1,23 @@
 var React = require('react/addons');
 
 module.exports = React.createClass({
+    getInitialState: function(){
+        return {
+            input: ""
+        }
+    },
+    handleInputName: function(e){
+        this.setState({input:e.target.value});
+    },
+    addItem: function(e){
+        e.preventDefault();
+        console.log('Adding Item!', this.state.input);
+    },
     render: function(){
         return (
             <div className="grocery-addItem">
-                <form>
-                    <input />
+                <form onSubmit={this.addItem}>
+                    <input value={this.state.input} onChange={this.handleInputName} />
                     <button>Add Item</button>
                 </form>
             </div>
